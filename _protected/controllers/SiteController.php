5299870d69723fd5ef1f634e27523c557eb3eb5c
root <ind_ari08@yahoo.com>
2020-02-06 13:29:09 +0700

    telegram bot

--- _protected/controllers/SiteController.php
+++ _protected/controllers/SiteController.php
@@ -10,9 +10,13 @@ use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
 use app\models\News;
+use yii\helpers\Url;
+use Exception;
 
 class SiteController extends Controller
 {
+    private $BOT_TOKEN = '1076405618:AAH2jsL8DbCKoMdjgXPY7lzacoIwoQBFGNY';
+
     /**
      * {@inheritdoc}
      */
@@ -91,6 +95,55 @@ class SiteController extends Controller
      */
     public function actionLogin()
     {
+        // function getTelegramUserData() {
+        //     if (isset($_COOKIE['tg_user'])) {
+        //       $auth_data_json = urldecode($_COOKIE['tg_user']);
+        //       $auth_data = json_decode($auth_data_json, true);
+        //       return $auth_data;
+        //     }
+        //     return false;
+        //   }
+          
+        //   if ($_GET['logout']) {
+        //     setcookie('tg_user', '');
+        //     header('Location: login_example.php');
+        //   }
+          
+        //   $tg_user = getTelegramUserData();
+        //   if ($tg_user !== false) {
+        //     $first_name = htmlspecialchars($tg_user['first_name']);
+        //     $last_name = htmlspecialchars($tg_user['last_name']);
+        //     if (isset($tg_user['username'])) {
+        //       $username = htmlspecialchars($tg_user['username']);
+        //       $html = "<h1>Hello, <a href=\"https://t.me/{$username}\">{$first_name} {$last_name}</a>!</h1>";
+        //     } else {
+        //       $html = "<h1>Hello, {$first_name} {$last_name}!</h1>";
+        //     }
+        //     if (isset($tg_user['photo_url'])) {
+        //       $photo_url = htmlspecialchars($tg_user['photo_url']);
+        //       $html .= "<img src=\"{$photo_url}\">";
+        //     }
+        //     $html .= "<p><a href=\"?logout=1\">Log out</a></p>";
+        //   } else {
+        //     $bot_username = 'sec2bot';
+        //     $html = <<<HTML
+        //   <h1>Hello, anonymous!</h1>
+        //   <script async src="https://telegram.org/js/telegram-widget.js?2" data-telegram-login="{$bot_username}" data-size="large" data-auth-url="check_authorization.php"></script>
+        //   HTML;
+        //   }
+          
+          
+        //     echo <<<HTML
+        //   <!DOCTYPE html>
+        //   <html>
+        //     <head>
+        //       <meta charset="utf-8">
+        //       <title>Login Widget Example</title>
+        //     </head>
+        //     <body><center>{$html}</center></body>
+        //   </html>
+        //   HTML;
+          
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
@@ -143,6 +196,45 @@ class SiteController extends Controller
      */
     public function actionAbout()
     {
+        try {
+            $auth_data = $this->checkTelegramAuthorization($_GET);
+            $this->saveTelegramUserData($auth_data);
+            // Url::to('login');
+            return $this->render('about');
+          } catch (Exception $e) {
+            die ($e->getMessage());
+          }
+
         return $this->render('about');
     }
+
+    /**
+     * Telegram Bot 
+     */
+    private function checkTelegramAuthorization($auth_data) 
+    {
+        $check_hash = $auth_data['hash'];
+        unset($auth_data['hash']);
+        $data_check_arr = [];
+        foreach ($auth_data as $key => $value) {
+          $data_check_arr[] = $key . '=' . $value;
+        }
+        sort($data_check_arr);
+        $data_check_string = implode("\n", $data_check_arr);
+        $secret_key = hash('sha256', $this->BOT_TOKEN, true);
+        $hash = hash_hmac('sha256', $data_check_string, $secret_key);
+        if (strcmp($hash, $check_hash) !== 0) {
+          throw new Exception('Data is NOT from Telegram');
+        }
+        if ((time() - $auth_data['auth_date']) > 86400) {
+          throw new Exception('Data is outdated');
+        }
+        return $auth_data;
+      }
+      
+    private function saveTelegramUserData($auth_data) {
+        $auth_data_json = json_encode($auth_data);
+        setcookie('tg_user', $auth_data_json);
+    }
+      
 }
--- _protected/views/site/about.php
+++ _protected/views/site/about.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <code><?= __FILE__ ?></code>
 
 </div>
-<script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="cobabot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+<script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="sec2bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
 <script type="text/javascript">
   function onTelegramAuth(user) {
     alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
